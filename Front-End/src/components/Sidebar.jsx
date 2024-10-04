3e714da444c34e7c7cd14b24c7f3b8ef433eda3c
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2024-10-04 07:26:58 +0300

    Add files via upload

---
+++ Front-End/src/components/Sidebar.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable react/prop-types */
+import {
+  MdDashboard,
+  MdOutlinePendingActions,
+  MdSettings,
+  MdTaskAlt,
+} from "react-icons/md";
+import { FaTasks, FaTrashAlt, FaUsers } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { setOpenSidebar } from "../redux/slices/authSlice";
+import clsx from "clsx";
+import { assets } from "../assets/assets";
+
+const linkData = [
+  {
+    label: "Dashboard",
+    link: "dashboard",
+    icon: <MdDashboard />,
+  },
+  {
+    label: "Tasks",
+    link: "tasks",
+    icon: <FaTasks />,
+  },
+  {
+    label: "Completed",
+    link: "completed/completed",
+    icon: <MdTaskAlt />,
+  },
+  {
+    label: "In Progress",
+    link: "in-progress/in progress",
+    icon: <MdOutlinePendingActions />,
+  },
+  {
+    label: "To Do",
+    link: "todo/todo",
+    icon: <MdOutlinePendingActions />,
+  },
+  {
+    label: "Team",
+    link: "team",
+    icon: <FaUsers />,
+  },
+  {
+    label: "Trash",
+    link: "trashed",
+    icon: <FaTrashAlt />,
+  },
+];
+
+const Sidebar = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const path = location.pathname.split("/")[1];
+
+  const sidebarLinks = user?.isAdmin ? linkData : linkData.slice(0, 5);
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  };
+
+  const NavLink = ({ el }) => {
+    return (
+      <Link
+        to={el.link}
+        onClick={closeSidebar}
+        className={clsx(
+          "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800 text-base hover:bg-[#2564ed2d] hover:text-gray-800",
+          path === el.link.split("/")[0] ? "bg-[#6b43dd] text-neutral-100" : ""
+        )}
+      >
+        {el.icon}
+        <span className="hover:text-[#6b43dd]">{el.label}</span>
+      </Link>
+    );
+  };
+  return (
+    <div className="w-full  h-full flex flex-col gap-6 p-5">
+      <img className="w-[185px] p-2" src={assets.LogoH} alt="" />
+
+      <div className="flex-1 flex flex-col gap-y-5 py-8">
+        {sidebarLinks.map((link) => (
+          <NavLink el={link} key={link.label} />
+        ))}
+      </div>
+
+      <div className="">
+        <button className="w-full flex gap-2 p-2 items-center text-lg text-gray-800">
+          <MdSettings />
+          <span>Settings</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
