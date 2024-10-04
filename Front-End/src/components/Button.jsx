894eef5c1864bfc757adb82a78ad3d040d5ef629
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2024-10-04 07:25:48 +0300

    Add files via upload

---
+++ Front-End/src/components/Button.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import clsx from "clsx";
+
+const Button = ({ icon, className, label, type, onClick = () => {} }) => {
+  return (
+    <button
+      type={type || "button"}
+      className={clsx("px-3 py-2 outline-none", className)}
+      onClick={onClick}
+    >
+      <span>{label}</span>
+      {icon && icon}
+    </button>
+  );
+};
+
+export default Button;
