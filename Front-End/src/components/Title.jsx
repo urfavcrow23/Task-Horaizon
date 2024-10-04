801ffc12828e380ed70f4fac5919af266b63ba58
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2024-10-04 23:00:42 +0300

    Add files via upload

---
+++ Front-End/src/components/Title.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import clsx from "clsx";
+
+
+const Title = ({ title, className }) => {
+  return (
+    <h2 className={clsx("text-2xl font-semibold capitalize", className)}>
+      {title}
+    </h2>
+  );
+};
+
+export default Title;
