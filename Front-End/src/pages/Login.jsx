8e9642d122bd9ff693538fbc71327395dbb9cb9b
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2024-10-04 07:05:48 +0300

    Update Login.jsx

--- Front-End/src/pages/Login.jsx
+++ Front-End/src/pages/Login.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import Textbox from "../components/Textbox";
-import Button from "../components/Button";
 import { useSelector } from "react-redux";
+import { assets } from "../assets/assets";
+import { IoLogoApple } from "react-icons/io5";
+import { FaGoogle } from "react-icons/fa";
+import { useEffect } from "react";
 
 const Login = () => {
   const { user } = useSelector((state) => state.auth);
@@ -25,75 +27,94 @@ const Login = () => {
   }, [user]);
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
-      <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
-        {/* left side */}
-        <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
-          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
-            <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600">
-              Manage all your task in one place!
-            </span>
-            <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700">
-              <span>Cloud-Based</span>
-              <span>Task Manager</span>
-            </p>
-
-            <div className="cell">
-              <div className="circle rotate-in-up-left"></div>
-            </div>
+    <div className="px-40 py-6 flex gap-6 h-screen">
+      <div className="flex flex-col  basis-1/2 px-12 rounded-2xl justify-between  ">
+        <img className="w-52 pt-12" src={assets.LogoV} alt="" />
+        <div className="">
+          <h1 className="font-bold text-3xl  pb-1">Login to Dashboard</h1>
+          <p className="font-medium pb-6">Fill the bellow form to login</p>
+          <div className="flex justify-between">
+            <button className="flex gap-2 py-2 px-6 size-fit  border-2 rounded-md justify-center items-center hover:text-white hover:bg-[#6b43dd] transition-all duration-300">
+              <FaGoogle size={22} />
+              <p>Sign in with Google</p>
+            </button>
+            <button className="flex gap-2 py-2 px-6 size-fit  border-2 rounded-md justify-center items-center hover:text-white hover:bg-[#6b43dd] transition-all duration-300">
+              <IoLogoApple size={24} />
+              <p>Sign in with Apple</p>
+            </button>
           </div>
         </div>
 
-        {/* right side */}
-        <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
-          <form
-            onSubmit={handleSubmit(submitHandler)}
-            className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14"
+        <div className="flex items-center space-x-2 ">
+          <hr className="flex-grow border-gray-300" />
+          <p>OR</p>
+          <hr className="flex-grow border-gray-300 " />
+        </div>
+        <form
+          className=" flex flex-col justify-start items-start gap-6"
+          action=""
+          onSubmit={handleSubmit(submitHandler)}
+        >
+          <div className="w-full">
+            <Textbox
+              placeholder="email@example.com"
+              type="email"
+              name="email"
+              label="Email Address"
+              className="w-full rounded-lg"
+              register={register("email", {
+                required: "Email Address is required!",
+              })}
+              error={errors.email ? errors.email.message : ""}
+            />
+          </div>
+          <div className="w-full">
+            <Textbox
+              placeholder="your password"
+              type="password"
+              name="password"
+              label="Password"
+              className="w-full rounded-lg"
+              register={register("password", {
+                required: "Password is required!",
+              })}
+              error={errors.password ? errors.password.message : ""}
+            />
+          </div>
+          <button className="font-bold text-[#6b43dd] hover:text-[#35148f] hover:underline">
+            Forgot Password?
+          </button>
+          <button
+            type="submit"
+            label="Submit"
+            className="mb-12 w-full bg-[#6b43dd] text-white font-semibold text-lg p-2 rounded-lg hover:bg-[#35148f] transition-all duration-300"
           >
-            <div className="">
-              <p className="text-blue-600 text-3xl font-bold text-center">
-                Welcome back!
-              </p>
-              <p className="text-center text-base text-gray-700 ">
-                Keep all your credential safge.
-              </p>
-            </div>
+            Login
+          </button>
+        </form>
+      </div>
 
-            <div className="flex flex-col gap-y-5">
-              <Textbox
-                placeholder="email@example.com"
-                type="email"
-                name="email"
-                label="Email Address"
-                className="w-full rounded-full"
-                register={register("email", {
-                  required: "Email Address is required!",
-                })}
-                error={errors.email ? errors.email.message : ""}
-              />
-              <Textbox
-                placeholder="your password"
-                type="password"
-                name="password"
-                label="Password"
-                className="w-full rounded-full"
-                register={register("password", {
-                  required: "Password is required!",
-                })}
-                error={errors.password ? errors.password.message : ""}
-              />
+      <div className="relative flex flex-col overflow-hidden basis-1/2 bg-violet-100 rounded-2xl p-12">
+        <div className="flex flex-col gap-6 z-10">
+          <div className="text-slate-600 border-2 border-slate-300 rounded-full inline-block p-4 bg-slate-100 shadow-md size-fit">
+            <p className="text-sm font-medium">
+              Manage all your tasks in one place
+            </p>
+          </div>
 
-              <span className="text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer">
-                Forget Password?
-              </span>
+          <h2 className="text-black text-3xl font-bold leading-relaxed ">
+            Manage your tasks and
+            <br /> Operations
+            <span className="text-[#6b43dd]"> more Professionally</span>
+          </h2>
+        </div>
 
-              <Button
-                type="submit"
-                label="Submit"
-                className="w-full h-10 bg-blue-700 text-white rounded-full"
-              />
-            </div>
-          </form>
+        <div className=" pl-16 absolute right-[-280px] bottom-[0px] z-0">
+          <img
+            className=" blur-[2px] rounded-t-xl object-cover w-[3641px] h-auto shadow-md  hover:blur-none  transition-all duration-1000"
+            src={assets.LogDashImg}
+            alt="Task Management Dashboard"
+          />
         </div>
       </div>
     </div>
